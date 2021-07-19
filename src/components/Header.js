a9252b88bd36f7a3d61b3211506871f2fd4e25e8
Francis Lesui <37851797+tflesui@users.noreply.github.com>
2021-07-18 21:47:08 -0700

    Added Login page, began Firebase setup.

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,10 +21,12 @@ const Header = () => {
       </div>
 
       <div className="header_nav">
-        <div className="header_option">
-          <span className="header_optionLineOne">Hello Guest</span>
-          <span className="header_optionLineTwo">Sign In</span>
-        </div>
+        <Link to="/login">
+          <div className="header_option">
+            <span className="header_optionLineOne">Hello Guest</span>
+            <span className="header_optionLineTwo">Sign In</span>
+          </div>  
+        </Link>        
         <div className="header_option">
           <span className="header_optionLineOne">Returns</span>
           <span className="header_optionLineTwo">& Orders</span>
